3ac64832ec4cd19faf37767f1b6a5f8324a88bf3
peikovakate <peikovakate@gmail.com>
2020-09-08 13:29:50 +0300

    update script for mergin queried sumstat

--- combine_query_sumstat_for_variants.R
+++ combine_query_sumstat_for_variants.R
@@ -1,8 +1,19 @@
-variants_folder = "../data2/sumstat/"
-output_file = "../data2/sumstat_from_cc/cc_eqtls.tsv"
+`%>%` <- magrittr::`%>%`
+
+parser <- optparse::OptionParser()
+parser <- optparse::add_option(parser, c("-f", "--folder"), 
+                               type = "character", 
+                               help="folder with variants files")
+parser <- optparse::add_option(parser, c('-o', '--output'),
+                               type="character", 
+                               help = "output filename tsv")
+args = optparse::parse_args(parser)
+
+variants_folder = args$folder
+output_file = args$output
 
 files = list.files(variants_folder)
-tbls = lapply(file.path(variants_folder, files), readr::read_tsv)
+tbls = lapply(file.path(variants_folder, files), readr::read_tsv, col_types = readr::cols())
 tbls = dplyr::bind_rows(tbls)
 head(tbls)
 
