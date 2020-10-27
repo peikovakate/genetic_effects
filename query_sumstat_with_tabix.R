521035b5f0896a1d695576cac971d9b9d89da0ba
peikovakate <peikovakate@gmail.com>
2020-10-27 18:21:27 +0200

    add comments

--- query_sumstat_with_tabix.R
+++ query_sumstat_with_tabix.R
@@ -38,6 +38,8 @@ coords_file = sprintf("%s_coords.tsv", sub('\\.tsv$', '', pairs_file))
 temp_sumstat_file = file.path(output_dir, paste0(qtl_group, "_temp.tsv"))
 
 variants = readr::read_tsv(pairs_file, col_types = readr::cols())
+
+# in case of file with variants has inconsitent name (microarray vs rna-seq)
 if("phenotype_id" %in% names(variants)){
   variants = dplyr::rename(variants, molecular_trait_id = phenotype_id)  
 }
@@ -45,9 +47,11 @@ if("variant_id" %in% names(variants)){
   variants = dplyr::rename(variants, variant = variant_id)
 }
 
+# set file with variant coordinates as input and redirect output of tabix to temporary file
 input = paste(sumstat_file, "-R", coords_file, ">", temp_sumstat_file)
 sumstat_out = system2(tabix_path, args=input, stdout = T)
 
+# read results in temp file
 sumstat = readr::read_tsv(temp_sumstat_file, col_names = eqtl_colnames, col_types = eqtl_col_types)
 
 sumstat = dplyr::inner_join(sumstat, 
@@ -56,7 +60,7 @@ sumstat = dplyr::inner_join(sumstat,
 sumstat = dplyr::mutate(sumstat, qtl_group = qtl_group)
 # remove duplicates with rsid
 sumstat = dplyr::select(sumstat, -rsid) %>%  dplyr::distinct(.keep_all = T)
-
+# delete temp file
 file.remove(temp_sumstat_file)
 
 print(qtl_group)
